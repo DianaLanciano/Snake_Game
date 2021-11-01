b6af92b73f35dcf683e4231b2bcd487258ecaf89
Diana <isakovdiana1@gmail.com>
2021-11-01 19:56:23 +0200

    set directions to snake

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,17 +6,16 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
+public class GamePanel extends JPanel implements ActionListener {
 
-public class GamePanel extends JPanel  implements ActionListener {
-
-	//panel size
+	// panel size
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
-	//How big are the items on screen
+	// How big are the items on screen
 	static final int UNIT_SIZE = 25;
-	
-	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-	static final int DELEY = 75;
+
+	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+	static final int DELEY = 70;
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
 	int bodyParts = 6;
@@ -27,103 +26,154 @@ public class GamePanel extends JPanel  implements ActionListener {
 	boolean running = false;
 	Timer timer;
 	Random random;
-	
-	GamePanel(){
+
+	GamePanel() {
 		random = new Random();
 		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		this.setBackground(Color.black);
 		this.setFocusable(true);
 		this.addKeyListener(new MyKeyAdaptor());
 		startGame();
-		
+
 	}
-	
+
 	public void startGame() {
 		newApple();
 		running = true;
 		timer = new Timer(DELEY, this);
 		timer.start();
-		
+
 	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
 	}
+
 	public void draw(Graphics g) {
-		int loopSize = SCREEN_HEIGHT/UNIT_SIZE;
-		for(int i=0; i < loopSize; i++) {
-			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+		int loopSize = SCREEN_HEIGHT / UNIT_SIZE;
+		for (int i = 0; i < loopSize; i++) {
+			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
 		}
 		g.setColor(Color.red);
 		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-		
-		for(int i = 0; i < bodyParts; i++) {
-			if(i == 0) {
+
+		for (int i = 0; i < bodyParts; i++) {
+			if (i == 0) {
 				g.setColor(Color.green);
 				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			}
-			else {
+			} else {
 				g.setColor(Color.blue);
 				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 			}
 		}
 	}
+
 	public void newApple() {
-		/*The purpose of this method is to generate the coordinates
-		of new apple when we call this method*/
-		
-		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
-		appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
-		
+		/*
+		 * The purpose of this method is to generate the coordinates of new apple when
+		 * we call this method
+		 */
+
+		appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+		appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+
 	}
+
 	public void move() {
-		for(int i = bodyParts; i>0; i--) {
-			x[i] = x[i-1];
-			y[i] = y[i-1];
+		for (int i = bodyParts; i > 0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
 		}
-		
+
 		switch (direction) {
-		case 'U': 
+		case 'U':
 			y[0] = y[0] - UNIT_SIZE;
 			break;
-		case 'D': 
+		case 'D':
 			y[0] = y[0] + UNIT_SIZE;
 			break;
-		case 'L': 
+		case 'L':
 			x[0] = x[0] - UNIT_SIZE;
 			break;
-		case 'R': 
+		case 'R':
 			x[0] = x[0] + UNIT_SIZE;
 			break;
-			
-			
+
 		}
 	}
+
 	public void checkApple() {
-		
+
 	}
+
 	public void checkCollisions() {
-		
+		// checks if head collides with body
+		for (int i = bodyParts; i > 0; i--) {
+			if ((x[0] == x[i]) && (y[0] == y[i])) {
+				running = false;
+			}
+		}
+		// checks if head touched left border
+		if (x[0] < 0) {
+			running = false;
+		}
+		// checks if head touched right border
+		if (x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+		//checks if head touched bottom border
+		if (y[0] < 0) {
+			running = false;
+		}
+		//checks if head touched top border
+				if (y[0] > SCREEN_HEIGHT) {
+					running = false;
+				}
+
 	}
+
 	public void gameOver(Graphics g) {
-		
+
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(running) {
+		if (running) {
 			move();
 			checkApple();
 			checkCollisions();
 		}
 		repaint();
-		
+
 	}
-	
-	public class MyKeyAdaptor extends KeyAdapter{
+
+	public class MyKeyAdaptor extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction != 'R')
+					direction = 'L';
+				break;
+				
+			case KeyEvent.VK_RIGHT:
+				if(direction != 'L')
+					direction = 'R';
+				break;
+				
+			case KeyEvent.VK_UP:
+				if(direction != 'D')
+					direction = 'U';
+				break;
+				
+			case KeyEvent.VK_DOWN:
+				if(direction != 'U')
+					direction = 'D';
+				break;
+			}
+
 		}
 	}
 
